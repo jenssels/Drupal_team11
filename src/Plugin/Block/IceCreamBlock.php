819fd6c7ddf97ca49c46bb7a7c5617be8862341e
Ricardo van Zeijl <ricardo.vanzeijl@gmail.com>
2018-05-24 11:40:57 +0200

    Block + Module Changed

--- src/Plugin/Block/IceCreamBlock.php
+++ src/Plugin/Block/IceCreamBlock.php
@@ -3,13 +3,13 @@
 namespace Drupal\thomas_more_ice_cream\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\State\StateInterface;
-use Drupal\thomas_more_ice_cream\ClickManager;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+//use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+//use Drupal\Core\State\StateInterface;
+//use Drupal\thomas_more_ice_cream\DatabaseManager;
+//use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Defines a social menu block.
+ * Defines an ice cream menu block.
  *
  * @Block(
  *  id = "thomas_more_ice_cream_block",
@@ -17,27 +17,27 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 
-class IceCreamBlock  extends BlockBase implements ContainerFactoryPluginInterface {
-  protected $clickManager;
-
-  protected $state;
-
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, StateInterface $state, ClickManager $clickManager) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->clickManager = $clickManager;
-    $this->state = $state;
-  }
-
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('state'),
-      $container->get('thomas_more_ice_cream.click_manager')
-    );
-  }
+class IceCreamBlock extends BlockBase {
+//  protected $databaseManager;
+//
+//  protected $state;
+//
+//  public function __construct(array $configuration, $plugin_id, $plugin_definition, StateInterface $state, DatabaseManager $databaseManager) {
+//    parent::__construct($configuration, $plugin_id, $plugin_definition);
+//
+//    $this->databaseManager = $databaseManager;
+//    $this->state = $state;
+//  }
+//
+//  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+//    return new static(
+//      $configuration,
+//      $plugin_id,
+//      $plugin_definition,
+//      $container->get('state'),
+//      $container->get('thomas_more_ice_cream.database_manager')
+//    );
+//  }
 
   /**
    * {@inheritdoc}
@@ -45,11 +45,11 @@ class IceCreamBlock  extends BlockBase implements ContainerFactoryPluginInterfac
   public function build() {
     return [
       '#theme' => 'social-media',
-      '#attached' => ['library' => ['thomas_more_ice_cream/ice_cream']],
-      '#ice_cream_tastes' => $this->state->get('thomas_more_ice_cream.ice_cream_tastes'),
-      '#ice_cream_count' => $this->clickManager->getClicks('ice_cream'),
-      '#waffle_toppings' => $this->state->get('thomas_more_ice_cream.waffle_toppings'),
-      '#waffle_count' => $this->clickManager->getClicks('waffle'),
+//      '#attached' => ['library' => ['thomas_more_ice_cream/ice_cream']],
+//      '#ice_cream_tastes' => $this->state->get('thomas_more_ice_cream.ice_cream_tastes'),
+//      '#ice_cream_count' => $this->clickManager->getClicks('ice_cream'),
+//      '#waffle_toppings' => $this->state->get('thomas_more_ice_cream.waffle_toppings'),
+//      '#waffle_count' => $this->clickManager->getClicks('waffle'),
     ];
   }
 
