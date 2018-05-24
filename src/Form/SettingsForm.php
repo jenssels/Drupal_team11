2f93933bb0c9b6347a4e263a212bf085fd594a12
StoopArno <stoop.arno@gmail.com>
2018-05-24 10:47:40 +0200

    Routes aangepast, SettingsForm toegevoegd

---
+++ src/Form/SettingsForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\thomas_more_ice_cream\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\State\StateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class SettingsForm extends FormBase {
+
+  protected $state;
+
+  public function __construct(StateInterface $state) {
+    $this->state = $state;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('state')
+    );
+  }
+
+  public function getFormId() {
+    return 'thomas_more_ice_cream_settings_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state){
+    $form['ijsjes_threshold'] = [
+      '#type' => 'number',
+      '#title' => 'Threshold voor de ijsjes.',
+      '€default_value' => $this->state->get('thomas_more_ice_cream.ijsjes_threshold')
+    ];
+
+    $form['wafels_threshold'] = [
+      '#type' => 'number',
+      '#title' => 'Threshold voor de wafels.',
+      '#default_value' => $this->state->get('thomas_more_ice_cream.wafels_threshold')
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => 'Opslaan',
+      '#button_type' => 'primary',
+    ];
+
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+}
