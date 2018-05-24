fb1fa80f74b6937e02b3a633331021841dd2c42d
Ricardo van Zeijl <ricardo.vanzeijl@gmail.com>
2018-05-24 14:28:50 +0200

    Block Changed

--- src/Plugin/Block/IceCreamBlock.php
+++ src/Plugin/Block/IceCreamBlock.php
@@ -3,10 +3,6 @@
 namespace Drupal\thomas_more_ice_cream\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-//use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-//use Drupal\Core\State\StateInterface;
-//use Drupal\thomas_more_ice_cream\DatabaseManager;
-//use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines an ice cream menu block.
@@ -18,39 +14,15 @@ use Drupal\Core\Block\BlockBase;
  */
 
 class IceCreamBlock extends BlockBase {
-//  protected $databaseManager;
-//
-//  protected $state;
-//
-//  public function __construct(array $configuration, $plugin_id, $plugin_definition, StateInterface $state, DatabaseManager $databaseManager) {
-//    parent::__construct($configuration, $plugin_id, $plugin_definition);
-//
-//    $this->databaseManager = $databaseManager;
-//    $this->state = $state;
-//  }
-//
-//  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-//    return new static(
-//      $configuration,
-//      $plugin_id,
-//      $plugin_definition,
-//      $container->get('state'),
-//      $container->get('thomas_more_ice_cream.database_manager')
-//    );
-//  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    return [
-      '#theme' => 'social-media',
-//      '#attached' => ['library' => ['thomas_more_ice_cream/ice_cream']],
-//      '#ice_cream_tastes' => $this->state->get('thomas_more_ice_cream.ice_cream_tastes'),
-//      '#ice_cream_count' => $this->clickManager->getClicks('ice_cream'),
-//      '#waffle_toppings' => $this->state->get('thomas_more_ice_cream.waffle_toppings'),
-//      '#waffle_count' => $this->clickManager->getClicks('waffle'),
-    ];
+
+    $form = \Drupal::formBuilder()->getForm('Drupal\thomas_more_ice_cream\Form\IceCreamForm');
+
+    return $form;
   }
 
 
